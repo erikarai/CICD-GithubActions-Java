4984ef3ac526b2989bab1992af07524feb69e334
Erika Rai <erika.rai@incedoinc.com>
2023-08-23 09:47:38 +0530

    v1.1.3

--- main.tf
+++ main.tf
@@ -11,15 +11,15 @@ terraform {
  
 
 provider "aws" {
-  access_key = "AKIASHELXOYST4ECEYN6"
-  secret_key = "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"
+  access_key = "AKIA6ESCSUTQE7ST3BGV"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
  
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-21augbucket22"
+  bucket = "my-tf-test-23augbucket22"
   acl    = "private"
 
  
